93811ee6104f5ae275bfe291b59fc515466a1b55
Алексей Арх <info@arh.one>
2018-09-09 15:10:00 +0300

    dev

---
+++ source/CacheException.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace arhone\caching;
+
+/**
+ * Interface CacheException
+ * @package arhone\caching
+ * @author Алексей Арх <info@arh.one>
+ */
+interface CacheException {}
